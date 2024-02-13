c409294dd21286fb9d8308b9e411c54417b532b2
Daniel Corvalán <daniel@corvalan.dev>
2024-02-13 14:17:00 -0300

    with path

--- render.py
+++ render.py
@@ -1,12 +1,15 @@
 from jinja2 import Environment, FileSystemLoader
+import argparse
 
+parser = argparse.ArgumentParser()
+fpath = parser.parse_args()
 env = Environment(loader=FileSystemLoader('templates'))
 templates = ['index.html', 'planes.html', 'programas.html', 'espacio.html', 'terminos.html', '404.html', 'nosotros.html']
 
 for template in templates:
     tmp = env.get_template(template)
     out = tmp.render()
-    location = "./_site/{}".format(template)
+    location = fpath + "{}".format(template)
     with open(location, "w", encoding="utf-8") as f:
         f.write(out)
 print("Listo")
